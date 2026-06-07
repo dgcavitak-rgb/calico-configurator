bb29873c8d5d475ec601b7c0eff9308e0b16ce43
dgcavitak-rgb <dg.cavitak@gmail.com>
2026-06-07 12:29:04 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -78,7 +78,7 @@
 // the activate handler. Promotes hard-refresh semantics for users with the
 // PWA installed.
 
-const CACHE_NAME = 'tvone-v27.25.1';
+const CACHE_NAME = 'tvone-v27.26.0';
 const SHELL_URLS = [
   './',
   './index.html'
